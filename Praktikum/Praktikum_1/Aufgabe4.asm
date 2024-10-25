4f9940268258fde4d9bdc47ea6cccc8c95ef1c33
M-0-K <62558735+M-0-K@users.noreply.github.com>
2024-10-25 19:27:49 +0200

    PR01 Beendet

--- Praktikum/Praktikum_1/Aufgabe4.asm
+++ Praktikum/Praktikum_1/Aufgabe4.asm
@@ -2,15 +2,24 @@ org 100h
 
 ;-------------------------------
 leds: equ  0x00 
-start:   mov al, 0x90
+start:   
+mov ah, 0
+int 6
 
-clear:
-out 0x90, 0
-out leds, 0
-add al, 0x02
+//0 Speichern und Anzeigen
+mov al, 0b00111111
+out 0x90, al 
+//delay s
+mov cx,-1
+schl1: loop schl1
 
-cmp al, 0x9e
-jne clear
+//1 Speichern und Anzeigen
+mov al, 0x06
+out 0x90, al 
+
+//delay 
+mov cx,-1
+schl2: loop schl2
 
 
 jmp start 
